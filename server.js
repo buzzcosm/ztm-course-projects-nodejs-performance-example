baa91abc931b7b06d76d567e3811a83624f7215f
Jeff_Buzz <97390468+buzzcosm@users.noreply.github.com>
2024-11-07 21:21:52 +0100

    Add cluster case with real cpu num. workers to increase server performance

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cluster = require('cluster');
+const os = require('os');
 
 const app = express();
 
@@ -15,15 +17,24 @@ app.get('/', (req, res) => {
   // JSON.stringify({}) => "{}"
   // JSON.parse("{}") => {}
   // [5,4,3,2,1].sort();
-  res.send('Performance example');
+  res.send(`Performance example: ${process.pid}`);
 });
 
 app.get('/timer', (req, res) => {
   delay(9000);
-  res.send('Ding ding ding!');
+  res.send(`Ding ding ding! ${process.pid}`);
 });
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000');
-});
+console.log(`Running server.js...`);
+if (cluster.isMaster) {
+  console.log(`Master has been started...`);
+  const NUM_WORKERS = os.cpus().length;
+  for (let i = 0; i < NUM_WORKERS; i++) {
+    cluster.fork();
+  }
+} else {
+  console.log(`Worker process started.`);
+  app.listen(3000);
+}
+
 
